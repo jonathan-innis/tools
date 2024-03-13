8fb2615828cba3bb3814145d9a27f430d3180d3f
Jason Deal <jmdeal@amazon.com>
2024-03-13 09:42:17 -0700

    make image script generic

--- get-ubuntu-images/main.go
+++ get-ubuntu-images/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"sort"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
@@ -13,53 +15,127 @@ import (
 	"github.com/samber/lo"
 )
 
+type Options struct {
+	Regions []string
+	Images  []string
+	OutPath string
+}
+
+func ParseFlags() (*Options, error) {
+	regionsString := flag.String("regions", "", "A comma separated list of regions to query. If unspecified, queries all commercial regions.")
+	imagesString := flag.String("images", "", "A comma separated list of image names to query.")
+	outputPath := flag.String("out", "ami_ids.csv", "File to write the results.")
+	flag.Parse()
+
+	opts := &Options{}
+	opts.Regions = lo.Ternary(lo.FromPtr(regionsString) == "", []string{
+		"af-south-1",
+		"ap-east-1",
+		"ap-northeast-1",
+		"ap-northeast-2",
+		"ap-northeast-3",
+		"ap-south-1",
+		"ap-south-2",
+		"ap-southeast-1",
+		"ap-southeast-2",
+		"ap-southeast-3",
+		"ca-central-1",
+		"ca-west-1",
+		"eu-central-1",
+		"eu-central-2",
+		"eu-north-1",
+		"eu-south-1",
+		"eu-south-2",
+		"eu-west-1",
+		"eu-west-2",
+		"eu-west-3",
+		"il-central-1",
+		"me-central-1",
+		"me-south-1",
+		"sa-east-1",
+		"us-east-1",
+		"us-east-2",
+		"us-west-1",
+		"us-west-2",
+	}, strings.Split(lo.FromPtr(regionsString), ","))
+	if lo.FromPtr(imagesString) == "" {
+		return nil, fmt.Errorf("must specify --images")
+	}
+	opts.Images = strings.Split(lo.FromPtr(imagesString), ",")
+	opts.OutPath = lo.FromPtr(outputPath)
+	return opts, nil
+}
+
+type AMIEntry struct {
+	Name   string
+	ID     string
+	Region string
+}
+
 func main() {
+	opts, err := ParseFlags()
+	if err != nil {
+		panic(fmt.Errorf("parsing flags, %w", err))
+	}
+
 	ctx := context.Background()
 	cfg := lo.Must(config.LoadDefaultConfig(ctx))
+	ec2Client := ec2.NewFromConfig(cfg)
 
-	file := lo.Must(os.OpenFile("ami-ids.csv", os.O_RDWR|os.O_CREATE, 0777))
+	file := lo.Must(os.OpenFile(opts.OutPath, os.O_RDWR|os.O_CREATE, 0777))
 	defer file.Close()
-
 	w := csv.NewWriter(file)
 	defer w.Flush()
 
-	regions := []string{"us-west-2", "us-east-1", "us-east-2", "eu-west-1"}
-	ec2Client := ec2.NewFromConfig(cfg)
-	for _, version := range []string{"1.23", "1.24", "1.25", "1.26", "1.27", "1.28"} {
-		rows := [][]string{}
-		imageMap := map[string]map[string]string{}
-		for _, region := range regions {
-			out := lo.Must(ec2Client.DescribeImages(ctx, &ec2.DescribeImagesInput{
-				Filters: []types.Filter{
-					{
-						Name: lo.ToPtr("name"),
-						Values: []string{
-							fmt.Sprintf("ubuntu-eks/k8s_%s/images/hvm-ssd/ubuntu-focal-20.04-arm64-server*", version),
-							fmt.Sprintf("ubuntu-eks/k8s_%s/images/hvm-ssd/ubuntu-focal-20.04-amd64-server*", version),
-						},
-					},
+
+	failedRegions := []string{}
+	entries := []AMIEntry{}
+	for _, region := range opts.Regions {
+		out, err := ec2Client.DescribeImages(ctx, &ec2.DescribeImagesInput{
+			Filters: []types.Filter{
+				{
+					Name:   lo.ToPtr("name"),
+					Values: opts.Images,
 				},
-			}, WithRegion(region)))
-			for _, img := range out.Images {
-				if _, ok := imageMap[lo.FromPtr(img.Name)]; !ok {
-					imageMap[lo.FromPtr(img.Name)] = map[string]string{}
-				}
-				imageMap[lo.FromPtr(img.Name)][region] = lo.FromPtr(img.ImageId)
-			}
+			},
+		}, WithRegion(region))
+		if err != nil {
+			failedRegions = append(failedRegions, region)
+			continue
 		}
-		for k, v := range imageMap {
-			row := []string{k}
-			for _, r := range regions {
-				row = append(row, v[r])
+		entries = append(entries, lo.Map(out.Images, func(image types.Image, _ int) AMIEntry {
+			return AMIEntry{
+				Name:   lo.FromPtr(image.Name),
+				ID:     lo.FromPtr(image.ImageId),
+				Region: region,
 			}
-			rows = append(rows, row)
+		})...)
+	}
+
+	sort.Slice(entries, func(ii, ji int) bool {
+		i := entries[ii]
+		j := entries[ji]
+		if i.Name != j.Name {
+			return i.Name < j.Name
 		}
-		sort.Slice(rows, func(i, j int) bool {
-			return rows[i][0] < rows[j][0]
-		})
-		rows = append([][]string{{version}, append([]string{""}, regions...)}, rows...)
-		rows = append(rows, []string{})
-		w.WriteAll(rows)
+		if i.Region != j.Region {
+			return i.Region < j.Region
+		}
+		return i.ID < j.ID
+	})
+
+	if err := w.Write([]string{"ami_name", "ami_id", "region"}); err != nil {
+		panic(fmt.Errorf("writing csv, %w", err))
+	}
+	if err := w.WriteAll(lo.Map(entries, func(entry AMIEntry, _ int) []string {
+		return []string{entry.Name, entry.ID, entry.Region}
+	})); err != nil {
+		panic(fmt.Errorf("writing csv, %w", err))
+	}
+
+	fmt.Printf("queried %d AMIs across %d regions\n", len(entries), len(opts.Regions) - len(failedRegions))
+	if len(failedRegions) != 0 {
+		fmt.Printf("failed to query AMIs for the following regions: %s\n", strings.Join(failedRegions, ", "))
 	}
 }
 
